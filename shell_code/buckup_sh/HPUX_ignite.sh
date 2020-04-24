ac229968518b3cbb6873f22e730a1296ae9c4775
chenlu121 <cl121@sina.com>
2020-04-24 08:11:34 +0800

    ''

--- shell_code/buckup_sh/HPUX_ignite.sh
+++ shell_code/buckup_sh/HPUX_ignite.sh
@@ -1,4 +1,4 @@
-root@bj1recap01:/home/space02/software/nbu/script> cat HPUX_ignite.sh
+root@bj1recap01:/home/space02/software/nbu/script >cat HPUX_ignite.sh
 #!/bin/ksh
 #$Header: HPUX_ignite.sh, v 1.0 2013/9/5 20:58 $
 #
@@ -10,40 +10,36 @@ root@bj1recap01:/home/space02/software/nbu/script> cat HPUX_ignite.sh
 #
 #
 #-----------------------------------------------------------------------
-export DATE=`date +"%Y%m%d%H%M%S"`
+export DATE=$(date +"%Y%m%d%H%M%S")
 export ALIAS=
 export IPADDR=
 export FS=
 
-bk_flag()
-{
-echo
-echo "########################################################################"
-echo
+bk_flag() {
+        echo
+        echo "########################################################################"
+        echo
 }
 
-info()
-{
-echo
-echo "-------------------------------------------------------------------"
-echo
-echo "$1"
-echo
-echo "-------------------------------------------------------------------"
-echo
+info() {
+        echo
+        echo "-------------------------------------------------------------------"
+        echo
+        echo "$1"
+        echo
+        echo "-------------------------------------------------------------------"
+        echo
 }
 
-line()
-{
-echo "------------------------------------------------"
+line() {
+        echo "------------------------------------------------"
 }
 
-OS_backup()
-{
-case $OS in
-(HP-UX)
-mkdir -p  /var/opt/ignite/scripts
-echo "umask 022
+OS_backup() {
+        case $OS in
+        HP-UX)
+                mkdir -p /var/opt/ignite/scripts
+                echo "umask 022
 export ALIAS=$ALIAS
 export FS=$FS
 export LOGFILE=/var/opt/ignite/scripts/ignite_\${ALIAS}.log
@@ -54,43 +50,39 @@ RSTAT=\`cat \$RETURN\`
         then
                 RSTAT=0
         fi
-exit \$RSTAT" > /var/opt/ignite/scripts/ignite.sh
+exit \$RSTAT" >/var/opt/ignite/scripts/ignite.sh
 
-chmod 744 /var/opt/ignite/scripts/*.sh
+                chmod 744 /var/opt/ignite/scripts/*.sh
 
-IGNITE_OLD_VERSION_LIST=`swlist |grep -i ignite |awk '{print $1}' |uniq`
-for IGNITE_OLD_VERSION in $IGNITE_OLD_VERSION_LIST
-do
-/usr/sbin/swremove $IGNITE_OLD_VERSION
-done
-RET=$?
-if [ $RET -ne 0 ]
-then
-        info "The IGNITE_OLD_VERSION can not be swremoved,please use command "swlist" to check it first"
-        exit 3
-else
-        info "SWREMOVE OF IGNITE_OLD_VERSION complete!"
-fi
+                IGNITE_OLD_VERSION_LIST=$(swlist | grep -i ignite | awk '{print $1}' | uniq)
+                for IGNITE_OLD_VERSION in $IGNITE_OLD_VERSION_LIST; do
+                        /usr/sbin/swremove $IGNITE_OLD_VERSION
+                done
+                RET=$?
+                if [ $RET -ne 0 ]; then
+                        info "The IGNITE_OLD_VERSION can not be swremoved,please use command "swlist" to check it first"
+                        exit 3
+                else
+                        info "SWREMOVE OF IGNITE_OLD_VERSION complete!"
+                fi
 
-printf "begin install ignite of version 7.11.444"
+                printf "begin install ignite of version 7.11.444"
 
-/usr/sbin/swinstall -x mount_all_filesystems=false -s  npbj1ignbk01:/var/opt/ignite/depots/recovery_cmds "*"
+                /usr/sbin/swinstall -x mount_all_filesystems=false -s npbj1ignbk01:/var/opt/ignite/depots/recovery_cmds "*"
 
-RET=$?
-if [ $RET -ne 0 ]
-then
-        info "The IGNITE with version 7.11.44 can not be swinstalled,please use command "swlist" to check it"
-        exit 3
-else
-        info "SWINSTALL of ignite with version 7.11.444 complete!Check it by yourself"
-        echo `swlist |grep -i ignite`
-fi
-;;
-esac
+                RET=$?
+                if [ $RET -ne 0 ]; then
+                        info "The IGNITE with version 7.11.44 can not be swinstalled,please use command "swlist" to check it"
+                        exit 3
+                else
+                        info "SWINSTALL of ignite with version 7.11.444 complete!Check it by yourself"
+                        echo $(swlist | grep -i ignite)
+                fi
+                ;;
+        esac
 }
 
-
-export OS=`uname -s`
+export OS=$(uname -s)
 
 echo "Please input the  Client alias:"
 echo
@@ -108,18 +100,21 @@ echo "    \n Please choose the filesystem(1 or 2):\n
            \c"
 read Choice
 case "$Choice" in
-   1) FS=otherfs01 ;;
-   2) FS=otherfs02 ;;
-   *) echo "Invalid option"; exit 
+1) FS=otherfs01 ;;
+2) FS=otherfs02 ;;
+*)
+        echo "Invalid option"
+        exit
+        ;;
 esac
 
-echo  "Edit hosts file"
+echo "Edit hosts file"
 cp /etc/hosts /etc/hosts.$DATE
 echo "### BKMS server $DATE modified start
 11.155.61.27 npbj1ignbk01
 11.155.61.28 npbj1ignbk02
 $IPADDR $ALIAS
 ### BKMS server $DATE modified end
-" >> /etc/hosts
+" >>/etc/hosts
 
 OS_backup
--- shell_code/buckup_sh/nim_create_client.sh
+++ shell_code/buckup_sh/nim_create_client.sh
@@ -1,4 +1,4 @@
-sfmon@bj1recap01:/home/space02/software/nbu/script/nim_create> cat nim_create_client.sh
+sfmon@bj1recap01:/home/space02/software/nbu/script/nim_create >cat nim_create_client.sh
 #!/bin/ksh
 #$Header: nim_create_client.sh, v 1.0 2013/12/23 14:00 $
 #
@@ -14,48 +14,44 @@ sfmon@bj1recap01:/home/space02/software/nbu/script/nim_create> cat nim_create_cl
 #ecopyright
 #
 export script_dir=/mnt/mnt5/nbu/script/nim_create
-export HOSTNAME=`hostname`
+export HOSTNAME=$(hostname)
 export LOG=/tmp/nim_create_$HOSTNAME.log
-export DATE=`date +"%Y%m%d%H%M%S"`
+export DATE=$(date +"%Y%m%d%H%M%S")
 export IPADD=$1
 export ALIAS=$2
 #export ALIAS=$(echo $2 |tr "[:upper:]" "[:lower:]"|sed -e 's/_//g')
 
-hosts_modify()
-{
-cp  /etc/hosts  /etc/hosts.$DATE
-echo "### NIM server $DATE modified start
+hosts_modify() {
+      cp /etc/hosts /etc/hosts.$DATE
+      echo "### NIM server $DATE modified start
 #NIM Server
 11.155.61.25   lpbj1nimbk01
 11.155.61.26   lpbj1nimbk02
 $IPADD $ALIAS
 ### NIM server $DATE modified end
-" >> /etc/hosts
+" >>/etc/hosts
 }
 
-nim_install()
-{
+nim_install() {
 
-niminit -a master=lpbj1nimbk01 -a name=$ALIAS  -a connect=nimsh  >/dev/null 2>&1
-NIMSH=`lssrc -s nimsh | grep nimsh | awk '{print $NF}'`
-echo
-echo "NIMSH status is $NIMSH"
-echo
-if [ "$NIMSH" == "active" ]
-        then
-        echo "*******************************************"
-        echo
-        echo "/etc/niminfo  configration list as follows:"
-        cat /etc/niminfo  | grep NIM_NAME            | awk '{print $2}'
-        cat /etc/niminfo  | grep NIM_HOSTNAME        | awk '{print $2}'
-        cat /etc/niminfo  | grep NIM_MASTER_HOSTNAME | awk '{print $2}'
-        echo
-        echo "********************************************"
-        else
-        echo " Client $ALIAS nimsh status is not active,Please recheck!"
-        exit $EXIT
-fi
-
+      niminit -a master=lpbj1nimbk01 -a name=$ALIAS -a connect=nimsh >/dev/null 2>&1
+      NIMSH=$(lssrc -s nimsh | grep nimsh | awk '{print $NF}')
+      echo
+      echo "NIMSH status is $NIMSH"
+      echo
+      if [ "$NIMSH" == "active" ]; then
+            echo "*******************************************"
+            echo
+            echo "/etc/niminfo  configration list as follows:"
+            cat /etc/niminfo | grep NIM_NAME | awk '{print $2}'
+            cat /etc/niminfo | grep NIM_HOSTNAME | awk '{print $2}'
+            cat /etc/niminfo | grep NIM_MASTER_HOSTNAME | awk '{print $2}'
+            echo
+            echo "********************************************"
+      else
+            echo " Client $ALIAS nimsh status is not active,Please recheck!"
+            exit $EXIT
+      fi
 
 }
 
@@ -64,15 +60,11 @@ umask 022
 touch /tmp/nim_create_$HOSTNAME.log
 chmod 744 /tmp/nim_create_$HOSTNAME.log
 
+NET=$(netstat -in | grep "$IPADD" | awk '{print $1}' | cut -c 3-4)
+default=$(netstat -rn | grep "default" | awk '{print $2}')
+ipadd=$(netstat -in | grep "$IPADD" | awk '{print $4}')
 
-
-
-    NET=$(netstat -in | grep "$IPADD"   | awk  '{print $1}' |cut -c 3-4)
-default=$(netstat -rn | grep "default"  | awk  '{print $2}')
-  ipadd=$(netstat -in | grep "$IPADD"   | awk  '{print $4}')
-
-
-echo  "Clinet information confirms:"
+echo "Clinet information confirms:"
 echo
 echo "NIM_CLIENT_ALIAS       :         $ALIAS"
 echo "NIM_CLIENT_IP          :         $ipadd"
@@ -83,9 +75,8 @@ cd /tmp
 rm -f /tmp/$ALIAS.$NET
 echo 1>/tmp/$ALIAS.$NET
 
-if [ -f "/tmp/$ALIAS.$NET" ]
- then
-ftp -n  <<!
+if [ -f "/tmp/$ALIAS.$NET" ]; then
+      ftp -n <<!
 open 11.155.61.25
 user nimftp nimftp
 bin
@@ -99,15 +90,13 @@ fi
 
 sleep 20
 
-ls -al /etc/niminfo >/dev/null  2>&1
-        if [ $? -eq 0 ]
-        then
-        stopsrc -s nimsh  >/dev/null 2>&1
-        mv /etc/niminfo /etc/niminfo.bak
-              hosts_modify  | tee -a  $LOG
-              nim_install   | tee -a  $LOG
-        else
-              hosts_modify  | tee -a  $LOG
-              nim_install   | tee -a  $LOG
-        fi
-
+ls -al /etc/niminfo >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+      stopsrc -s nimsh >/dev/null 2>&1
+      mv /etc/niminfo /etc/niminfo.bak
+      hosts_modify | tee -a $LOG
+      nim_install | tee -a $LOG
+else
+      hosts_modify | tee -a $LOG
+      nim_install | tee -a $LOG
+fi
