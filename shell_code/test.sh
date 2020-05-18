9b954dd039d309fe077b12ac3e03f4877b65b03d
chenlu121 <cl121@sina.com>
2020-05-18 09:39:24 +0800

    ‘’

--- shell_code/test.sh
+++ shell_code/test.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 set -xeuo pipefail
+cat bpplist_bj1nbubk11_20191104010001.txt|sed -n  '/^Policy Name\|Schedule/p'
+
+#cat bpplist_bj1nbubk11_20191104010001.txt|sed -n '/Schedule/p'|wc -l
