dc3693142616241ce02703abcfa260a7e444e66a
chenlu121 <cl121@sina.com>
2020-07-23 13:11:20 +0800

    ‘’

--- shell_code/pip_upgrade.sh
+++ shell_code/pip_upgrade.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in `pip list --outdated|awk '{print $1}'|sed -n '3,$p'`
 do
-    pip install --upgrade $i|tee -a upgrade.log
+    pip install --upgrade $i 2>&1|tee -a upgrade.log
 done
