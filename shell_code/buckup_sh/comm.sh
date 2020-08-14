14a57609ce4781580028493ed8d4bbac3eb0a9c4
陈璐_test <cl_test@192.168.0.106>
2020-08-14 23:10:58 +0800

    ''

--- shell_code/buckup_sh/comm.sh
+++ shell_code/buckup_sh/comm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for i in `cat tclist.txt`
 do
-    echo  "pairdisplay -IH10 -CLI -fxcme -g $i">>comm.txt
+    echo  "pairvolchk -IH10 -g $i -ss">>comm.txt
 done
 echo  "\n">>comm.txt
 echo  "\n">>comm.txt
