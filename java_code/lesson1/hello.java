e4d5240af15d146e0dee97ee8afa0c5a55697e3c
chenlu121 <cl121@sina.com>
2020-12-04 09:23:02 +0800

    ''

--- java_code/lesson1/hello.java
+++ java_code/lesson1/hello.java
@@ -2,6 +2,7 @@ package java_code.lesson1;
 
 public class hello {
     public static void main(String[] args) {
-        System.out.println("hello world");        
+        double a = 3.14;
+        System.out.println(a);        
     }
 }
