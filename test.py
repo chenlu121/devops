efca07fb2082c38402add0799770188766d11ef6
cl121@sina.com <cl121@sina.com>
2025-10-04 20:52:38 +0800

    “Add test.py”

---
+++ test.py
@@ -0,0 +1,16 @@
+def fibonacci(n):
+    """生成斐波那契数列的前n项"""
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    
+    fib = [0, 1]
+    for i in range(2, n):
+        fib.append(fib[-1] + fib[-2])
+    return fib
+
+# 示例：打印前10项斐波那契数列
+print(fibonacci(10))
